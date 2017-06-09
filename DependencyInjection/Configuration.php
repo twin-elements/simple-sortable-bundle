d6d57de2f1e51773e45feb67d6f3c6d45c7d950a
jellinek <wladyslaw@jellinek.pl>
2017-06-09 09:48:58 +0200

    update dependency files

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TwinElements\SortablesBundle\DependencyInjection;
+namespace TwinElements\SortableBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
--- DependencyInjection/TwinElementsSortableExtension.php
+++ DependencyInjection/TwinElementsSortableExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use TwinElements\SortablesBundle\DependencyInjection\Configuration;
+//use TwinElements\SortableBundle\DependencyInjection\Configuration;
 
 /**
  * This is the class that loads and manages your bundle configuration.
